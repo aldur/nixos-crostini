cc3daae8a5d1785ff6aa3c86826024b46d7d2a11
Karolina Liskova <developer@0xADD1E.me>
2025-11-20 17:31:49 +0000

    Work within set -el for home-manager's initialisation script

--- common.nix
+++ common.nix
@@ -85,7 +85,7 @@ in
     };
 
     # Load the environment populated from `sommelier`, e.g. `DISPLAY`.
-    shellInit = builtins.readFile "${cros-container-guest-tools-src}/cros-sommelier/sommelier.sh";
+    shellInit = builtins.readFile ./sommelier.sh;
   };
 
   system.activationScripts = {
