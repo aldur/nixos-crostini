1372fb282ec7c66915ef2a5f14efa79f7c51f67a
Martin Kleinberger <martin.kleinberger@gmail.com>
2025-11-24 00:13:07 +0000

    Enhance flake.nix with description and outputs
    Added description field and updated outputs structure.

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Martin's Baguette Configuration";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-generators = {
@@ -6,42 +8,43 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs =
-    {
-      nixos-generators,
-      nixpkgs,
-      self,
-      ...
-    }@inputs:
-    let
-      modules = [ ./configuration.nix ];
 
-      # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-and-module-system
+  outputs = { self, nixpkgs, nixos-generators, ... }@inputs:
+    let
+      # --- CONFIGURATION ---
+      # We explicitly set this to x86_64 for Chrome OS Flex
+      system = "x86_64-linux";
+      
+      modules = [ ./configuration.nix ];
       specialArgs = { inherit inputs; };
 
-      # https://ayats.org/blog/no-flake-utils
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-      ];
-
-      targetSystem = "x86_64-linux";
-
+      # We manually define this to avoid any confusion. 
+      # ONLY x86_64 is allowed.
+      supportedSystems = [ "x86_64-linux" ];
+      
+      # Helper to apply logic to our one supported system
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
+      # --- PACKAGES (The build outputs) ---
       packages = forAllSystems (system: rec {
+        
+        # 1. LXC Generator
         lxc = nixos-generators.nixosGenerate {
           inherit system specialArgs modules;
           format = "lxc";
         };
+        
+        # 2. LXC Metadata
         lxc-metadata = nixos-generators.nixosGenerate {
           inherit system specialArgs modules;
           format = "lxc-metadata";
         };
 
+        # 3. Bundled LXC Image (for import)
         lxc-image-and-metadata = nixpkgs.legacyPackages.${system}.stdenv.mkDerivation {
           name = "lxc-image-and-metadata";
           dontUnpack = true;
-
           installPhase = ''
             mkdir -p $out
             ln -s ${lxc-metadata}/tarball/*.tar.xz $out/metadata.tar.xz
@@ -49,36 +52,42 @@
           '';
         };
 
+        # 4. Baguette VM Image (The one you want for ChromeOS 142)
         baguette-tarball = self.nixosConfigurations.baguette-nixos.config.system.build.tarball;
         baguette-image = self.nixosConfigurations.baguette-nixos.config.system.build.btrfsImage;
         baguette-zimage = self.nixosConfigurations.baguette-nixos.config.system.build.btrfsImageCompressed;
 
-        default = self.packages.${system}.lxc-image-and-metadata;
+        # Default build target
+        default = lxc-image-and-metadata;
       });
 
+      # --- CHECKS (What CI runs) ---
       checks = forAllSystems (system: {
-        inherit (self.outputs.packages.${system}) baguette-tarball lxc-image-and-metadata;
+        inherit (self.packages.${system}) baguette-tarball lxc-image-and-metadata;
       });
 
-      # This allows you to re-build the container from inside the container.
+      # --- SYSTEM CONFIGURATIONS ---
+      
+      # 1. The Container Config
       nixosConfigurations.lxc-nixos = nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
+        inherit system specialArgs;
         modules = modules ++ [ self.nixosModules.crostini ];
-        system = targetSystem;
       };
 
+      # 2. The Baguette VM Config
       nixosConfigurations.baguette-nixos = nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        system = targetSystem;
+        inherit system specialArgs;
         modules = modules ++ [ self.nixosModules.baguette ];
       };
 
-      nixosModules = rec {
+      # --- MODULE EXPORTS ---
+      nixosModules = {
         crostini = ./crostini.nix;
         baguette = ./baguette.nix;
-        default = crostini;
+        default = ./crostini.nix;
       };
 
+      # --- TEMPLATES ---
       templates.default = {
         path = self;
         description = "nixos-crostini quick start";
