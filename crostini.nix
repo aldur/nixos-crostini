4624c7efd96ba3f4a0541b0422bcf560c2d4d6c3
Martin Kleinberger <martin.kleinberger@gmail.com>
2025-11-24 03:05:39 +0000

    Refactor Crostini Nix configuration
    Refactor Crostini Nix configuration to streamline imports and settings.

--- crostini.nix
+++ crostini.nix
@@ -1,21 +1,19 @@
-{ modulesPath, lib, ... }:
-{
-  imports = [
-    # Load defaults for running in an lxc container.
-    # This is explained in: https://github.com/nix-community/nixos-generators/issues/79
-    "${modulesPath}/virtualisation/lxc-container.nix"
+{ config, pkgs, lib, ... }:
 
-    ./common.nix
+{
+  # Example Crostini-specific tweaks
+
+  # Use a lightweight desktop or just a shell-only system,
+  # depending on what you actually want.
+  services.xserver.enable = false;
+
+  # Helpful packages for debugging inside the container
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    curl
+    htop
   ];
 
-  # `boot.isContainer` implies NIX_REMOTE = "daemon"
-  # (with the comment "Use the host's nix-daemon")
-  # We don't want to use the host's nix-daemon.
-  environment.variables.NIX_REMOTE = lib.mkForce "";
-
-  # Suppress daemons which will vomit to the log about their unhappiness
-  systemd.services."console-getty".enable = false;
-  systemd.services."getty@".enable = false;
-
-  networking.hostName = lib.mkDefault "lxc-nixos";
+  # Crostini/container-specific settings can go here
 }
