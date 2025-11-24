bf3e31a38415ddc8d5d9f8c5be962c4f07a436b3
Martin Kleinberger <martin.kleinberger@gmail.com>
2025-11-24 00:23:52 +0000

    Refactor flake.nix for improved structure
    Refactor flake.nix to streamline outputs and configurations.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Martin's Baguette Configuration";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-generators = {
@@ -8,43 +6,42 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = { self, nixpkgs, nixos-generators, ... }@inputs:
+  outputs =
+    {
+      nixos-generators,
+      nixpkgs,
+      self,
+      ...
+    }@inputs:
     let
-      # --- CONFIGURATION ---
-      # We explicitly set this to x86_64 for Chrome OS Flex
-      system = "x86_64-linux";
-      
       modules = [ ./configuration.nix ];
+
+      # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-and-module-system
       specialArgs = { inherit inputs; };
 
-      # We manually define this to avoid any confusion. 
-      # ONLY x86_64 is allowed.
-      supportedSystems = [ "x86_64-linux" ];
-      
-      # Helper to apply logic to our one supported system
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      # https://ayats.org/blog/no-flake-utils
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+      ];
+
+      targetSystem = "x86_64-linux";
+
     in
     {
-      # --- PACKAGES (The build outputs) ---
       packages = forAllSystems (system: rec {
-        
-        # 1. LXC Generator
         lxc = nixos-generators.nixosGenerate {
           inherit system specialArgs modules;
           format = "lxc";
         };
-        
-        # 2. LXC Metadata
         lxc-metadata = nixos-generators.nixosGenerate {
           inherit system specialArgs modules;
           format = "lxc-metadata";
         };
 
-        # 3. Bundled LXC Image (for import)
         lxc-image-and-metadata = nixpkgs.legacyPackages.${system}.stdenv.mkDerivation {
           name = "lxc-image-and-metadata";
           dontUnpack = true;
+
           installPhase = ''
             mkdir -p $out
             ln -s ${lxc-metadata}/tarball/*.tar.xz $out/metadata.tar.xz
@@ -52,42 +49,36 @@
           '';
         };
 
-        # 4. Baguette VM Image (The one you want for ChromeOS 142)
         baguette-tarball = self.nixosConfigurations.baguette-nixos.config.system.build.tarball;
         baguette-image = self.nixosConfigurations.baguette-nixos.config.system.build.btrfsImage;
         baguette-zimage = self.nixosConfigurations.baguette-nixos.config.system.build.btrfsImageCompressed;
 
-        # Default build target
-        default = lxc-image-and-metadata;
+        default = self.packages.${system}.lxc-image-and-metadata;
       });
 
-      # --- CHECKS (What CI runs) ---
       checks = forAllSystems (system: {
-        inherit (self.packages.${system}) baguette-tarball lxc-image-and-metadata;
+        inherit (self.outputs.packages.${system}) baguette-tarball lxc-image-and-metadata;
       });
 
-      # --- SYSTEM CONFIGURATIONS ---
-      
-      # 1. The Container Config
+      # This allows you to re-build the container from inside the container.
       nixosConfigurations.lxc-nixos = nixpkgs.lib.nixosSystem {
-        inherit system specialArgs;
+        inherit specialArgs;
         modules = modules ++ [ self.nixosModules.crostini ];
+        system = targetSystem;
       };
 
-      # 2. The Baguette VM Config
       nixosConfigurations.baguette-nixos = nixpkgs.lib.nixosSystem {
-        inherit system specialArgs;
+        inherit specialArgs;
+        system = targetSystem;
         modules = modules ++ [ self.nixosModules.baguette ];
       };
 
-      # --- MODULE EXPORTS ---
-      nixosModules = {
+      nixosModules = rec {
         crostini = ./crostini.nix;
         baguette = ./baguette.nix;
-        default = ./crostini.nix;
+        default = crostini;
       };
 
-      # --- TEMPLATES ---
       templates.default = {
         path = self;
         description = "nixos-crostini quick start";
